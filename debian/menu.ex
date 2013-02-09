f7ba71fdbfde722ec850139b573cc63958fa078d
Devon Kearns <dookie@kali.org>
2013-02-09 06:11:14 -0700

    Pre-build commit

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(siparmyknife):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="siparmyknife" command="/usr/bin/siparmyknife"
--- debian/siparmyknife.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the siparmyknife package
-#
-0 4	* * *	root	[ -x /usr/bin/siparmyknife_maintenance ] && /usr/bin/siparmyknife_maintenance
